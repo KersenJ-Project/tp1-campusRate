ed4caa246ef6625f514117a90583df9f2eff7a9e
KersenJ-Project <nylekersenjoseph@gmail.com>
2026-09-19 23:32:02 -0400

    GET endpoints done

--- src/reviews/reviews.service.ts
+++ src/reviews/reviews.service.ts
@@ -1,23 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { Review } from './entities/review.entity';
 
 @Injectable()
 export class ReviewsService {
+  private reviews: Review[] = []
+
   create(createReviewDto: CreateReviewDto) {
     return 'This action adds a new review';
   }
 
   findAll() {
-    return `This action returns all reviews`;
+    return this.reviews;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} review`;
+  findOne(id: string) {
+    const review = this.reviews.find((r) => r.id === id)
+    if(!review){
+      throw new NotFoundException(`La critique avec l'id '${id}' n'existe pas.`)
+    }
+    return review
   }
 
-  update(id: number, updateReviewDto: UpdateReviewDto) {
-    return `This action updates a #${id} review`;
+  update(i: number, updateReviewDto: UpdateReviewDto) {
+    return `This action updates a #${i} review`;
   }
 
   remove(id: number) {
